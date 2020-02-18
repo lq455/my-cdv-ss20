836e02d9b0e3da54c882fc473dfd904a8c1e363e
Qin <qin@Qins-MacBook-Pro.local>
2020-02-18 11:18:32 +0800

    made changes to coding-foundation

--- my-work/week1/coding-foundation/main.js
+++ my-work/week1/coding-foundation/main.js
@@ -1,5 +1,5 @@
 var input = document.getElementById('input');
-var n=0
+
 var theDiv = document.getElementById("ip");
 
 
@@ -7,9 +7,9 @@ function getNumber(){
 
   num=input.value
   theDiv.innerHTML=''
-  n = 0
+
   for(i=0;i<num;i++){
-  n = i+1
+
   var btn = document.createElement("BUTTON");
 
 
